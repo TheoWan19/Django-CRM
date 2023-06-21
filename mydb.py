309c499cb77cb2e21cdc4df96f463a1bf773e6b1
TheoWan19 <theowan24@gmail.com>
2023-06-21 03:35:57 -0700

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+# Import mysql connector
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'Pewil',
+	)
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
